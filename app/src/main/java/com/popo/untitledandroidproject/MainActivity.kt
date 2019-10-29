8a95ca2f160eef9237e15a95af47fe8952815eae
Antoine <antoine.ganne@hotmail.fr>
2019-10-29 12:46:42 +0100

    [feature] main activity

---
+++ app/src/main/java/com/popo/untitledandroidproject/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.popo.untitledandroidproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivityMainBinding
+//    private val user:User= User(0,"Ganne","Steve")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
