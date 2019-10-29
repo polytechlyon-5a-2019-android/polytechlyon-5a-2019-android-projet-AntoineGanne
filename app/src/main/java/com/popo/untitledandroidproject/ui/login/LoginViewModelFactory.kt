901ccc4c234046f9c875521236037271c2a7af31
Antoine <antoine.ganne@hotmail.fr>
2019-10-30 00:26:22 +0100

    Update LoginViewModelFactory.kt

--- app/src/main/java/com/popo/untitledandroidproject/ui/login/LoginViewModelFactory.kt
+++ app/src/main/java/com/popo/untitledandroidproject/ui/login/LoginViewModelFactory.kt
@@ -1,24 +1,28 @@
 package com.popo.untitledandroidproject.ui.login
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.popo.untitledandroidproject.data.LoginDataSource
-import com.popo.untitledandroidproject.data.LoginRepository
-import com.popo.untitledandroidproject.viewmodel.LoginViewModel
+import com.popo.untitledandroidproject.database.UserDao
+import java.lang.IllegalArgumentException
 
 /**
  * ViewModel provider factory to instantiate LoginViewModel.
  * Required given LoginViewModel has a non-empty constructor
  */
-class LoginViewModelFactory : ViewModelProvider.Factory {
+class LoginViewModelFactory (
+    private val dataSource: UserDao,
+    private val application: Application,
+    private val userID: Long = 0L // userID
+) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
             return LoginViewModel(
-                loginRepository = LoginRepository(
-                    dataSource = LoginDataSource()
-                )
+                dataSource,
+                application,
+                userID
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
