b1c46e7637c58fb07bcd71e7ef1c836c17093a63
Antoine <antoine.ganne@hotmail.fr>
2019-12-12 12:01:57 +0100

    connection viewModel -Layout for MovieInfos

--- app/src/main/java/com/popo/untitledandroidproject/ui/movieInfo/MovieInfosViewModelFactory.kt
+++ app/src/main/java/com/popo/untitledandroidproject/ui/movieInfo/MovieInfosViewModelFactory.kt
@@ -1,4 +1,21 @@
 package com.popo.untitledandroidproject.ui.movieInfo
 
-class MovieInfosViewModelFactory {
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.popo.untitledandroidproject.model.Movie
+import java.lang.IllegalArgumentException
+
+@Suppress("UNCHECKED_CAST")
+class MovieInfosViewModelFactory(private val application: Application, private val movie: Movie): ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(MovieInfosViewModel::class.java)){
+            return MovieInfosViewModel(
+                application,
+                movie
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
